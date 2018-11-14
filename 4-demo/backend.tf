236a424b1ff733d7acc2a3a527d6f7aafd350191
Ned Bellavance <ned.bellavance@gmail.com>
2018-11-13 20:32:34 -0500

    4-demo

--- 4-demo/backend.tf
+++ 4-demo/backend.tf
@@ -1,8 +1,9 @@
 terraform {
-  backend "s3" {
-    key = "4-demo-network.state"
-    region = "us-east-1"
-    bucket = "vpcdemo10112018-remotestate"
-    dynamodb_table = "vpcdemo10112018-tfstatelock"
+  backend "azurerm" {
+    storage_account_name = "azug11142018remotestate"
+    container_name       = "azug11142018-remotestate-demos"
+    key                  = "4-demo.state"
+
+    resource_group_name = "azug11142018-setup"
   }
 }
--- 4-demo/datasources.tf
+++ 4-demo/datasources.tf
@@ -1,25 +1,3 @@
-data "template_file" "public_cidrsubnet" {
-  count = "${var.aws_subnet_count}"
-
-  template = "$${cidrsubnet(vpc_cidr,8,current_count)}"
-
-  vars {
-    vpc_cidr      = "${var.aws_network_address_space}"
-    current_count = "${count.index*2+1}"
-  }
-}
-
-data "template_file" "private_cidrsubnet" {
-  count = "${var.aws_subnet_count}"
-
-  template = "$${cidrsubnet(vpc_cidr,8,current_count)}"
-
-  vars {
-    vpc_cidr      = "${var.aws_network_address_space}"
-    current_count = "${count.index*2}"
-  }
-}
-
 data "template_file" "arm_cidrsubnet" {
   count = "${var.arm_subnet_count}"
 
