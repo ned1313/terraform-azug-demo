13c84eb2605a0a977de9f3fd3779f86ded85bbae
Ned Bellavance <ned.bellavance@gmail.com>
2018-11-13 20:51:08 -0500

    remove aws variables

--- 4-demo/variables.tf
+++ 4-demo/variables.tf
@@ -2,23 +2,6 @@
 # VARIABLES
 ##################################################################################
 
-#AWS variables
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_network_address_space" {
-  default = "10.1.0.0/16"
-}
-variable "aws_subnet1_address_space" {
-  default = "10.1.0.0/24"
-}
-variable "aws_subnet2_address_space" {
-  default = "10.1.1.0/24"
-}
-
-variable "aws_subnet_count" {
-  default = "2"
-}
-
 # Azure Variables
 variable "arm_region" {
   default = "eastus"
